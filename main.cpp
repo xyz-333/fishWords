34a80cbd6d8471fe1d683eb72a61cef5c6bc1e22
Przemysław Pełka <przemyslaw.j.pelka@gmail.com>
2017-05-21 00:04:46 +0200

    implemented loadWordList()

--- main.cpp
+++ main.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
+vector<string> wordList;
+
+void loadWordList();
+
 int main()
 {
-	cout << "Starting project fishWords..." << endl;
+	cout << ">> Starting project fishWords\n\n\n";
+	cout << ">> Loading list of words to memory..." << endl;
+	loadWordList();
 	return 0;
+}
+
+void loadWordList()
+{
+	char currentChar='a';
+	string currentWord;
+	ifstream subtitles;
+	subtitles.open("subtitles.srt", ios::in);
+	if(subtitles.good())
+	{
+		while(subtitles.get(currentChar))
+		{
+			if(isalpha(currentChar)) 																					// TODO: make it work for words with diacritics.
+				currentWord+=currentChar;
+			else
+			{
+				if(!currentWord.empty())
+				{
+					if(currentWord.length()>2&&currentChar!='\'')														// omits 1 and 2 letter words  and those with apostrophes
+						wordList.push_back(currentWord);
+					currentWord.clear();
+				}
+			}
+		}
+		subtitles.close();
+		cout << "<< Successfully loaded " << wordList.size() << " words from the subtitles." << endl;
+	}
+	else cerr << "<< Subtitles couldn't be read!" << endl;
 }
